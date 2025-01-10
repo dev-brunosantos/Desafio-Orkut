4cfa81351aa15f4b2a758a0edd60e06e95b2b70f
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-10 12:08:26 -0300

    Correção do tamanho da fonte

--- src/components/profile/cardProfile.tsx
+++ src/components/profile/cardProfile.tsx
@@ -8,7 +8,7 @@ interface CardProfileProps {
 export const CardProfile = ({ txt, children }: CardProfileProps) => {
     return (
         <div className="w-[130px] flex flex-col justify-between mr-[80px] relative">
-            <p className="text-[16px] font-normal text-[#4b4b4b]">
+            <p className="text-[14px] font-normal text-[#4b4b4b]">
                 {txt}
             </p>
             <div className="w-auto max-w-[46px] flex items-center gap-[6px]">
