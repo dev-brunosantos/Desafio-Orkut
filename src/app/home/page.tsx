b6a9d10492e25fad2b1aa29687960e4f496ce05b
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-10 09:29:56 -0300

    Adicionando componentes na tela HOME

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,4 +1,7 @@
+import { ContainerComunity } from "@/src/components/ContainerComunity";
+import { ContainerUsers } from "@/src/components/ContainerUsers";
 import { MenuPrincipal } from "@/src/components/MenuPrincipal";
+import { PerfilComponent } from "@/src/components/PerfilComponent";
 
 export default function Home() {
     return (
@@ -6,6 +9,19 @@ export default function Home() {
             <header className="w-[100%] flex items-center justify-center bg-dark40">
                 <MenuPrincipal />
             </header>
+
+            <main className="flex items-center justify-center">
+
+
+                <div className="w-[100%] max-w-[1240px] flex gap-8">
+                    <PerfilComponent />
+
+                    <div className="w-auto h-[815px] flex flex-col gap-8">
+                        <ContainerUsers />
+                        <ContainerComunity />
+                    </div>
+                </div>
+            </main>
         </div>
     )
 }
