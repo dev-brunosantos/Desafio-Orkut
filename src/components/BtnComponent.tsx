00a958e57a0a4e7e05d1a3e645becae6da4e87b5
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-10 14:27:08 -0300

    Simplificando o componente de Botão

--- src/components/BtnComponent.tsx
+++ src/components/BtnComponent.tsx
@@ -1,26 +1,18 @@
 "use client"
 
 import Link from "next/link";
-import { ButtonHTMLAttributes } from "react"
 
-interface BtnProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface BtnProps {
     titulo: string;
     btnPadrao: boolean;
     btnBranco?: boolean;
 }
 
-export const BtnComponent = ({ titulo, btnPadrao, btnBranco, ...rest }: BtnProps) => {
+export const BtnComponent = ({ titulo, btnPadrao, btnBranco }: BtnProps) => {
 
     if (btnPadrao) {
         return (
             <div className="w-[336px] h-[52px] my-2 rounded-lg gap-2 relative flex items-center justify-center bg-brandColor overflow-hidden" >
-                {/* <button
-                    {...rest}
-                    className="w-[100%] h-[100%] bg-transparent px-4 text-[16px] text-dark40 font-semibold "
-                >
-                    <Link href='home' className="w-[100%] h-[100%] z-100" >{titulo}</Link>
-                </button> */}
-
                 <Link href='home' className="w-[100%] h-[100%] bg-transparent px-4 text-[16px] text-dark40 font-semibold flex items-center justify-center " >
                     {titulo}
                 </Link>
@@ -31,24 +23,18 @@ export const BtnComponent = ({ titulo, btnPadrao, btnBranco, ...rest }: BtnProps
     if (btnBranco) {
         return (
             <div className="w-[100%] h-[52px] my-2 rounded-lg gap-2 flex items-center justify-center bg-dark40 overflow-hidden relative">
-                <button
-                    {...rest}
-                    className="w-[100%] h-[100%] bg-transparent px-4 text-[16px] text-brandColor font-semibold "
-                >
+                <Link href='home' className="w-[100%] h-[100%] bg-transparent px-4 text-[16px] text-brandColor font-semibold flex items-center justify-center " >
                     {titulo}
-                </button>
+                </Link>
             </div>
         )
     }
 
     return (
         <div className="w-[336px] h-[52px] my-2 rounded-lg gap-2 relative flex items-center justify-center bg-dark30 overflow-hidden">
-            <button
-                {...rest}
-                className="w-[100%] h-[100%] bg-transparent px-4 text-[16px] text-brandColor font-semibold "
-            >
+            <Link href='home' className="w-[100%] h-[100%] bg-transparent px-4 text-[16px] text-brandColor font-semibold flex items-center justify-center " >
                 {titulo}
-            </button>
+            </Link>
         </div>
     )
 }
