beb0013caf158bbec16d69bf9ccff9a6ba477590
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-10 10:53:32 -0300

    Aplicando componente CONTENT do perfil

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,16 +1,12 @@
-import { ContainerComunity } from "@/src/components/ContainerComunity";
-import { ContainerUsers } from "@/src/components/ContainerUsers";
+// import Image from "next/image";
+
 import { MenuPrincipal } from "@/src/components/MenuPrincipal";
 import { PerfilComponent } from "@/src/components/PerfilComponent";
-import Image from "next/image";
-
-import ImgFrase from '../../../public/frase-efeito.png'
-import Stars from '../../../public/img/svg/star.svg'
-import Smiley from '../../../public/img/svg/smiley.svg'
-import Like from '../../../public/img/svg/like.svg'
-import Heart from '../../../public/img/svg/heart.svg'
-import { CardProfile } from "@/src/components/profile/cardProfile";
-import { CardProfileStatus } from "@/src/components/profile/CardProfileStatus";
+// import { CardProfileStatus } from "@/src/components/profile/CardProfileStatus";
+// import { CardProfile } from "@/src/components/profile/CardProfile";
+import { ContainerUsers } from "@/src/components/ContainerUsers";
+import { ContainerComunity } from "@/src/components/ContainerComunity";
+import { ContentProfile } from "@/src/components/profile/ContentProfile";
 
 export default function Home() {
     return (
@@ -20,49 +16,11 @@ export default function Home() {
             </header>
 
             <main className="flex items-center justify-center">
-
-
                 <div className="w-[100%] max-w-[1240px] flex gap-8">
                     <PerfilComponent />
 
                     <div className="w-[592px] h-[868px] flex flex-col items-center rounded-2xl gap-6 bg-dark40">
-                        {/* CRIAR O COMPONENTE DE STATUS */}
-                        <div className="w-[544px] h-[184px] flex flex-col gap-10">
-                            <div className="w-[544px] h-[78px] gap-1">
-                                <p className="text-[20px] font-medium text-[#4B4B4B]">
-                                    Boa tarde, Iuri Silva
-                                </p>
-
-                                <Image src={ImgFrase} alt="" />
-                            </div>
-
-                            <CardProfileStatus />
-
-                            <div className="[346px] h-[46px] inline-flex">
-                                <CardProfile txt="Fã">
-                                    <Image src={Stars} alt="" />
-                                    <p className="text-[16px] text-[#4B4B4B] font-normal">
-                                        85
-                                    </p>
-                                </CardProfile>
-
-                                <CardProfile txt="Confiável">
-                                    <Image src={Smiley} alt="" />
-                                    <Image src={Smiley} alt="" />
-                                </CardProfile>
-
-                                <CardProfile txt="Legal">
-                                    <Image src={Like} alt="" />
-                                    <Image src={Like} alt="" />
-                                    <Image src={Like} alt="" />
-                                </CardProfile>
-
-                                <CardProfile txt="Sexy">
-                                    <Image src={Heart} alt="" />
-                                    <Image src={Heart} alt="" />
-                                </CardProfile>
-                            </div>
-                        </div>
+                        <ContentProfile />
                     </div>
 
                     <div className="w-auto h-[815px] flex flex-col gap-8">
