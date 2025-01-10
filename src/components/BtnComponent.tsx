9e5c5ebcf96ee3af8288ef5e6300ba414c4a9f79
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-10 09:28:50 -0300

    Criando uma nova condição para o uso do botão - novo modelo de botão

--- src/components/BtnComponent.tsx
+++ src/components/BtnComponent.tsx
@@ -6,9 +6,10 @@ import { ButtonHTMLAttributes } from "react"
 interface BtnProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     titulo: string;
     btnPadrao: boolean;
+    btnBranco?: boolean;
 }
 
-export const BtnComponent = ({ titulo, btnPadrao, ...rest }: BtnProps) => {
+export const BtnComponent = ({ titulo, btnPadrao, btnBranco, ...rest }: BtnProps) => {
 
     if (btnPadrao) {
         return (
@@ -23,6 +24,18 @@ export const BtnComponent = ({ titulo, btnPadrao, ...rest }: BtnProps) => {
         )
     }
 
+    if (btnBranco) {
+        return (
+            <div className="w-[100%] h-[52px] my-2 rounded-lg gap-2 flex items-center justify-center bg-dark40 overflow-hidden relative">
+                <button
+                    {...rest}
+                    className="w-[100%] h-[100%] bg-transparent px-4 text-[16px] text-brandColor font-semibold "
+                >
+                    {titulo}
+                </button>
+            </div>
+        )
+    }
 
     return (
         <div className="w-[336px] h-[52px] my-2 rounded-lg gap-2 relative flex items-center justify-center bg-dark30 overflow-hidden">
