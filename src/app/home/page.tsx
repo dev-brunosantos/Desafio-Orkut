179b20096f413b1eeb7bb795025d9cc843f7b3ea
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-10 11:59:09 -0300

    Aplicando as informações do usuário usando a constante de informações

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,6 +7,7 @@ import { PerfilComponent } from "@/src/components/PerfilComponent";
 import { ContainerUsers } from "@/src/components/ContainerUsers";
 import { ContainerComunity } from "@/src/components/ContainerComunity";
 import { ContentProfile } from "@/src/components/profile/ContentProfile";
+import { pessoaInfor } from "@/src/components/profile/inforPerfil";
 
 export default function Home() {
     return (
@@ -16,11 +17,25 @@ export default function Home() {
             </header>
 
             <main className="flex items-center justify-center">
-                <div className="w-[100%] max-w-[1240px] flex gap-8">
+                <div className="w-[100%] max-w-[1240px] flex  gap-8">
                     <PerfilComponent />
 
-                    <div className="w-[592px] h-[868px] flex flex-col items-center rounded-2xl gap-6 bg-dark40">
+                    <div className="w-[592px] h-[868px] py-8 px-6 flex flex-col items-start rounded-2xl gap-6 bg-dark40">
                         <ContentProfile />
+
+                        <div className="w-[405px] h-[596px] flex flex-col gap-6">
+                            {
+                                pessoaInfor.map(obj => (
+                                    <p key={obj.Chave} className="text-[16px] text-[#B4AEAE] font-normal">
+                                            {obj.Chave}
+                                            : 
+                                        <span className="text-[16px] text-[#4B4B4B] font-normal">
+                                            {obj.Valor}
+                                        </span>
+                                    </p>
+                                ))
+                            }
+                        </div>
                     </div>
 
                     <div className="w-auto h-[815px] flex flex-col gap-8">
