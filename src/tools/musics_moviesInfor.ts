bbf38b8fa30c56352cf871432333c4666ed1349f
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-10 13:51:15 -0300

    Criando constante com as informações da categoria musica e filmes

---
+++ src/tools/musics_moviesInfor.ts
@@ -0,0 +1,20 @@
+export const musics = [
+    {
+        "music": "Trap"
+    },
+    {
+        "music": "Rap"
+    },
+    {
+        "music": "Indie"
+    }
+]
+
+export const movies = [
+    {
+        "movie": "A rede Social"
+    },
+    {
+        "movie": "Meu amigo Toronto"
+    }
+]
