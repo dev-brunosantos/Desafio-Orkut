82bcc9d2f9cfa876eb1a96fe9eb4e3df9546fd7b
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-10 09:28:22 -0300

    Criando o menu pricipal - Tela HOME

--- src/components/MenuPrincipal.tsx
+++ src/components/MenuPrincipal.tsx
@@ -3,8 +3,10 @@
 import Image from "next/image"
 import Link from "next/link"
 import { FaChevronDown } from "react-icons/fa";
+import { FiSearch } from "react-icons/fi";
 
 import Logo from '../../public/img/svg/logo-orkut.svg'
+import LogoPerfil from '../../public/img/users/iuricode 1.png'
 
 export const MenuPrincipal = () => {
     return (
@@ -22,16 +24,31 @@ export const MenuPrincipal = () => {
                 </nav>
             </div>
 
-            <div className="w-[570px] flex ">
-                <input type="text" placeholder="Pesquise no Orkut" />
+            <div className="w-[560px]  flex gap-12">
+                <div className="w-[355px] h-9 rounded-lg overflow-hidden flex items-center gap-3 relative bg-dark30">
 
-                <div className="flex">
-                    {/* <Image src='' alt="" /> */}
+                    <FiSearch size={20} color="#B4AEAE"
+                        className="absolute left-2"
+                    />
 
-                    <p>Bruno Santos</p>
+                    <input type="text"
+                        placeholder="Pesquise no Orkut"
+                        className="w-[100%] h-[100%] outline-none bg-transparent text-[14px] 
+                            font-normal text-[##B4AEAE] pl-9 z-50"
+                    />
+                </div>
 
-                    <button>
-                        <FaChevronDown size={20} />
+                <div className="w-[157px] h-9 flex items-center gap-[14px]">
+                    <Image src={LogoPerfil} alt="" className="h-[90%]" />
+
+                    <div className="flex">
+                        <p className="w-24 h-[100%] text-[14px] ">
+                            Bruno Santos
+                        </p>
+                    </div>
+
+                    <button className="">
+                        <FaChevronDown size={15} color="#ed2590 " />
                     </button>
                 </div>
             </div>
